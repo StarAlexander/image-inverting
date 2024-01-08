8b28fbeea0a147cff3b2433663d9dc9d222db1bf
alexander <starchenckoal@yandex.ru>
2024-01-08 19:01:39 +0300

    png support

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,19 @@ import { invert } from "./pkg/sap.js";
 
 export default function App(){
     const [imageData,setImageData] = React.useState('')
+    const [ext,setExt] = React.useState('')
     const setImage = (e) => {
         if (e.target.files[0]) {
+            switch (e.target.files[0].name.split('.').at(-1)) {
+                case "jpg":
+                    setExt("jpg")
+                    break;
+                case "png":
+                    setExt("png")
+                    break;
+                default:
+                    return;
+            }
             const reader = new FileReader()
             reader.onload = (ev) => {
                 setImageData(ev.target.result)
@@ -23,10 +34,10 @@ export default function App(){
                             <input onChange={setImage} type="file" className="overflow-hidden w-0"/>
                             Choose
                         </label>
-                        <button disabled = {imageData?false:true} onClick={()=> setImageData(prev=>`data:image/jpg;base64, ${invert(prev.substring(prev.indexOf(',')+1).trim())}`)} className="rounded-sm p-2 hover:bg-slate-900 hover:text-white transition-all shadow-sm shadow-slate-300 bg-slate-100">Invert</button>
+                        <button disabled = {imageData?false:true} onClick={()=> setImageData(prev=>`data:image/jpg;base64, ${invert(prev.substring(prev.indexOf(',')+1).trim(),ext)}`)} className="rounded-sm p-2 hover:bg-slate-900 hover:text-white transition-all shadow-sm shadow-slate-300 bg-slate-100">Invert</button>
                 </div>
                 <div className={`max-w-md mx-auto mt-8 w-96 h-64 ${imageData?'':'shadow-md border'} `}>
-                    <img src={imageData} alt="There is no picture yet" className={`object-cover ${imageData?'':'hidden'}`}/>
+                    <img src={imageData} download = "new-image.jpg" alt="There is no picture yet" className={`object-cover ${imageData?'':'hidden'}`}/>
                 </div>
                 
             </div>
