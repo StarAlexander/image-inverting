8a45e2212e80a4afd576dc5fa8e5d3b1d73f55fc
alexander <starchenckoal@yandex.ru>
2024-01-12 18:35:56 +0300

    added resizing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { invert } from "./pkg/sap.js";
+import { invert,resize } from "./pkg/sap.js";
 
 export default function App(){
     const [imageData,setImageData] = React.useState('')
     const [ext,setExt] = React.useState('')
+    const [isResizing,setIsResizing] = React.useState(false)
+    const [width,setWidth] = React.useState(0)
+    const [height,setHeight] = React.useState(0)
     const setImage = (e) => {
         if (e.target.files[0]) {
             switch (e.target.files[0].name.split('.').at(-1)) {
@@ -29,14 +32,31 @@ export default function App(){
     }
 
     const invertData = () => {
-        const str = invert(imageData.substring(imageData.indexOf(',')+1).trim(),ext)
+        const str = invert(normalize(imageData),ext)
         if (str !== "Error") {
             setImageData(`data:image/${ext};base64, ${str}`)
         }
     }
+
+    const resize_with_meta = ()=>{
+        const str = resize(normalize(imageData),Number(width),Number(height),ext)
+        setImageData(`data:image/${ext};base64, ${str}`)
+    }
+    const normalize = (str) => str.substring(str.indexOf(',')+1).trim()
+    
     return (
         <div className="flex h-screen w-screen">
-        <div className="h-screen w-40 bg-black"></div>
+        <div className="h-screen w-40 bg-black space-y-4">
+            <div onClick={()=>setIsResizing(false)} className="w-full h-auto p-3 cursor-pointer text-white hover:bg-white hover:text-black transition-all">
+                <p className=" max-w-md mx-auto">Invert</p>
+            </div>
+            <div onClick={()=>setIsResizing(true)} className="w-full h-auto p-3 cursor-pointer text-white hover:bg-white hover:text-black transition-all">
+                <p className=" max-w-md mx-auto">Resize</p>
+            </div>
+            <div className="w-full h-auto p-3 cursor-pointer text-white hover:bg-white hover:text-black transition-all">
+                <p className=" max-w-md mx-auto">Crop</p>
+            </div>
+        </div>
             <div className="max-w-md mx-auto space-y-8">
                 <h1 className="max-w-md mx-auto text-4xl pl-10 font-mono">Image inverting</h1>
                 <div className="flex max-w-md mx-auto justify-between">
@@ -45,12 +65,22 @@ export default function App(){
                             Choose
                         </label>
                         <button  onClick={()=>setImageData('')} className={` ${imageData?'':'hidden'} rounded-sm p-2 hover:bg-slate-900 hover:text-white transition-all shadow-sm shadow-slate-300 bg-slate-100`}>Clear</button>
-                        <button  onClick={invertData} className={` ${imageData?'':'hidden'} rounded-sm p-2 hover:bg-slate-900 hover:text-white transition-all shadow-sm shadow-slate-300 bg-slate-100`}>Invert</button>
+                        <button  onClick={invertData} className={` ${imageData && !isResizing?'':'hidden'} rounded-sm p-2 hover:bg-slate-900 hover:text-white transition-all shadow-sm shadow-slate-300 bg-slate-100`}>Invert</button>
                 </div>
-                <div className={`max-w-md mx-auto mt-8 w-96 h-64 ${imageData?'':'shadow-md border'} `}>
-                    <img src={imageData} download = "new-image.jpg" alt="There is no picture yet" className={`object-cover ${imageData?'':'hidden'}`}/>
-                </div>
+                {isResizing && 
+                <div className="flex p-3  bg-slate-200  justify-between space-x-2 w-full rounded-md">
+                <p className="font-mono p-1">width:</p>
+                <input type="number" value={width} min={0} onChange={(e)=>setWidth(e.target.value)} className="outline-none border-b w-12 border-black"/>
+                <p className="font-mono p-1">height:</p>
+                <input type="number" value={height} min={0} onChange={(e)=>setHeight(e.target.value)} className="outline-none border-b w-12 border-black"/>
+                <button onClick={resize_with_meta} className="bg-black text-white hover:bg-white hover:text-black rounded-sm transition-all p-2">apply</button>
+            </div>
+            }
                 
+                <div className={`max-w-md mx-auto mt-8 w-96 h-64 ${imageData?'':'shadow-md border'} `}>
+                    <img src={imageData} download = "new-image.jpg" alt="There is no picture yet" className={`${imageData?'':'hidden'} object-contain`}/>
+                </div>
+                <button className={`w-full ${imageData? '':'hidden'} bg-black text-white rounded-sm transition-all p-2 hover:bg-slate-100 hover:text-black`}><a download={"image"} href={imageData}>Save</a></button>
             </div>
         </div>
     )
--- src/lib.rs
+++ src/lib.rs
@@ -15,16 +15,38 @@ pub fn img_from_base(s:&str) -> DynamicImage {
     .unwrap()
 }
 
+pub fn encode_with_extension(img:&mut DynamicImage, mut buf: Vec<u8>, f:&str) -> String {
+    match f {
+        "jpg" => img.write_to(&mut Cursor::new(&mut buf), image::ImageOutputFormat::Jpeg(100)).unwrap(),
+        "png" => img.write_to(&mut Cursor::new(&mut buf), image::ImageOutputFormat::Png).unwrap(),
+        "bmp" => img.write_to(&mut Cursor::new(&mut buf), image::ImageOutputFormat::Bmp).unwrap(),
+        _ => {
+            return "Error".to_string();
+        }
+    }
+    let res = general_purpose::STANDARD.encode(&buf);
+    res
+}
+
 #[wasm_bindgen]
 pub fn invert(base_string:&str,f:&str) -> String {
     let mut img = img_from_base(base_string);
 
     imageops::invert(&mut img);
+    let buf = vec![];
+    encode_with_extension(&mut img, buf, f)
+}
+
+
+#[wasm_bindgen]
+pub fn resize(base_string:&str,width:u32,height:u32,f:&str) -> String {
+    let img = img_from_base(base_string);
+    let resized = imageops::resize(&img, width,height,imageops::FilterType::Triangle);
     let mut buf = vec![];
     match f {
-        "jpg" => img.write_to(&mut Cursor::new(&mut buf), image::ImageOutputFormat::Jpeg(100)).unwrap(),
-        "png" => img.write_to(&mut Cursor::new(&mut buf), image::ImageOutputFormat::Png).unwrap(),
-        "bmp" => img.write_to(&mut Cursor::new(&mut buf), image::ImageOutputFormat::Bmp).unwrap(),
+        "jpg" => resized.write_to(&mut Cursor::new(&mut buf), image::ImageOutputFormat::Jpeg(100)).unwrap(),
+        "png" => resized.write_to(&mut Cursor::new(&mut buf), image::ImageOutputFormat::Png).unwrap(),
+        "bmp" => resized.write_to(&mut Cursor::new(&mut buf), image::ImageOutputFormat::Bmp).unwrap(),
         _ => {
             return "Error".to_string();
         }
